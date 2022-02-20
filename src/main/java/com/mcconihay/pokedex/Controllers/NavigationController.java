079e27a5ea7f4847c5e83d79acd2fbe207287224
Ian McConihay <mcconihay.ian92@gmail.com>
2022-02-19 18:32:18 -0600

    fixed

--- src/main/java/com/mcconihay/pokedex/Controllers/NavigationController.java
+++ src/main/java/com/mcconihay/pokedex/Controllers/NavigationController.java
@@ -2,9 +2,11 @@ package com.mcconihay.pokedex.Controllers;
 
 import com.mcconihay.pokedex.Models.Trainer;
 import com.mcconihay.pokedex.Repository.TrainerRepo;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class NavigationController {
 	private final TrainerRepo trainerDao;
 
@@ -17,5 +19,13 @@ public class NavigationController {
 		model.addAttribute("trainer", new Trainer());
 		return "templates/views/landing-page";
 	}
+
+	@GetMapping("/test")
+	public String viewTest(){
+		return "templates/views/test";
+	}
+
+
+
 
 }
